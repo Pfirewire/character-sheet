03c3c5b4770ae618a5265186e05f55e1edd61eb2
Stephen Pflug <stephen@pflug.us>
2022-09-22 13:24:21 -0500

    first commit

---
+++ src/test/java/com/pfirewire/charactersheet/CharacterSheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfirewire.charactersheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharacterSheetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
